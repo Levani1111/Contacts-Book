a04fce650423952c20fcc7c4de3e05d7e6f7c9fd
Levani Papashvili <papashvililevani@gmail.com>
2020-03-18 19:54:47 -0400

    create database and schema

---
+++ lib/settings.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE 'contact_book';
+CREATE TABLE 'contacts' (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    phone_number VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    birthday DATE
+);
