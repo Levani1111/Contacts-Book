a2c8e2dd31a3a10b9f689fc7518398e3cb364153
Levani Papashvili <papashvililevani@gmail.com>
2020-03-19 14:41:30 -0400

    Aded Persons

--- lib/app.py
+++ lib/app.py
@@ -33,7 +33,7 @@ Lilly = Person(
     last_name = 'Johnson',
     phone_number = '555-555-5053',
     email = 'princess@gmail.com',
-    bithday = '11/13/1999'
+    birthday = '11/13/1999'
 )
 Lilly.save()
 Bailey = Person(
@@ -44,6 +44,144 @@ Bailey = Person(
     birthday = '10/30/2002'
 )
 Bailey.save()
+Jeremy = Person(
+    first_name = 'Jeremy',
+    last_name = 'Johnson',
+    phone_number = '555-980-6578',
+    email = 'jeremypa@mail.com',
+    birthday = '24/09/1982'
+)
+Jeremy.save()
+Gio = Person(
+    first_name = 'Gio',
+    last_name = 'Niparishvili',
+    phone_number = '987-340-1429',
+    email = 'gionb@mail.com',
+    birthday = '06/07/1986'
+)
+Gio.save()
+Lia = Person(
+    first_name = 'Lia',
+    last_name = 'Rusishvili',
+    phone_number = '545-340-9842',
+    email = 'liarus@mail.com',
+    birthday = '11/04/1785'
+)
+Lia.save()
+Omari = Person(
+    first_name = 'Omari',
+    last_name = 'Ninoshvili',
+    phone_number = '986-120-1236',
+    email = 'omargs@mail.com',
+    birthday = '10/30/200'
+)
+Omari.save()
+Patta = Person(
+    first_name = 'Patta',
+    last_name = 'Borjomi',
+    phone_number = '678-340-9876',
+    email = 'pattalab@mail.com',
+    birthday = '09/30/2001'
+)
+Patta.save()
+Anna = Person(
+    first_name = 'Anna',
+    last_name = 'Mamodze',
+    phone_number = '571-897-3452',
+    email = 'annatya@mail.com',
+    birthday = '11/02/2009'
+)
+Anna.save()
+Giorgi = Person(
+    first_name = 'Giorgi',
+    last_name = 'Nonoiashvil',
+    phone_number = '555-876-0093',
+    email = 'Giorginan@mail.com',
+    birthday = '03/30/1982'
+)
+Giorgi.save()
+Natela = Person(
+    first_name = 'Natela',
+    last_name = 'Baliako',
+    phone_number = '555-120-1643',
+    email = 'natelabsd@mail.com',
+    birthday = '03/05/2008'
+)
+Natela.save()
+Dato = Person(
+    first_name = 'Dato',
+    last_name = 'Guraspashvili',
+    phone_number = '555-987-0964',
+    email = 'datojgf@mail.com',
+    birthday = '01/06/2010'
+)
+Dato.save()
+Nika = Person(
+    first_name = 'Nika',
+    last_name = 'Sakashvili',
+    phone_number = '555-090-1283',
+    email = 'nikaso@mail.com',
+    birthday = '07/30/1783'
+)
+Nika.save()
+Papuna = Person(
+    first_name = 'Papauna',
+    last_name = 'Papaskiri',
+    phone_number = '555-449-0089',
+    email = 'papunaeee@mail.com',
+    birthday = '10/01/2007'
+)
+Papuna.save()
+Maka = Person(
+    first_name = 'Maka',
+    last_name = 'Blaxamishvili',
+    phone_number = '555-298-0983',
+    email = 'makaiut@mail.com',
+    birthday = '10/03/2000'
+)
+Maka.save()
+Jabo = Person(
+    first_name = 'Jabo',
+    last_name = 'Rosiko',
+    phone_number = '555-987-9873',
+    email = 'noklass@mail.com',
+    birthday = '04/11/1975'
+)
+Jabo.save()
+Scoot = Person(
+    first_name = 'Scoot',
+    last_name = 'Dior',
+    phone_number = '155-654-9543',
+    email = 'scootri@mail.com',
+    birthday = '10/03/1891'
+)
+Scoot.save()
+Wako = Person(
+    first_name = 'Wako',
+    last_name = 'Mamuashvili',
+    phone_number = '555-876-1543',
+    email = 'mamunsk@mail.com',
+    birthday = '15/01/2000'
+)
+Wako.save()
+Mariami = Person(
+    first_name = 'Mariami',
+    last_name = 'Berianidze',
+    phone_number = '555-140-1643',
+    email = 'mariamioyb@mail.com',
+    birthday = '08/09/2008'
+)
+Mariami.save()
+Soso = Person(
+    first_name = 'Soso',
+    last_name = 'Bodare',
+    phone_number = '557-220-2643',
+    email = 'soson@mail.com',
+    birthday = '07/08/1988'
+)
+Soso.save()
+
+
 person = Person.get(Person.first_name == 'Levani')
 print(person.first_name)
 print(person.last_name)
